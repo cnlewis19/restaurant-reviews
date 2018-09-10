8ff5009cff3ec5d360129a9aa354ce43068c2fa1
Clare-Lewis <cnlewis19@gmail.com>
2018-09-10 12:56:17 -0400

    updated accessibility and responsiveness based on reviewer suggestions

--- js/main.js
+++ js/main.js
@@ -159,11 +159,11 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.alt = 'photo of restaurant';
+  image.alt = 'photo of the restaurant ' + restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -179,6 +179,7 @@ createRestaurantHTML = (restaurant) => {
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   more.tabIndex = '3';
+  more.setAttribute('aria-label', 'view details for ' + restaurant.name);
   li.append(more)
 
   return li
